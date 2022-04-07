3a453ce9cee96a2928b73fb25d906b5da0595960
reshad <you@example.com>
2022-04-07 04:43:27 +0400

    first commit

---
+++ 06-04-22.Homework/Models/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_04_22.Homework.Models
+{
+    class MyList<T>
+    {
+        public T[] nums;
+        public MyList()
+        {
+            nums = new T[0];
+        }
+        public T this[int index]
+        {
+            get { return nums[index]; }
+            set
+            {
+                nums[index] = value;
+            }
+        }
+
+        
+
+
+        
+    }
+}
---
+++ 06-04-22.Homework/Program.cs
@@ -0,0 +1,38 @@
+﻿using _06_04_22.Homework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_04_22.Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> number = new MyList<int>();
+            int[] num = new int[10];
+            num[0] = 5;
+            num[1] = 3;
+            num[2] = 6;
+            num[3] = 7;
+            Resize(ref num, num.Length + 1);
+            Console.WriteLine(num.Length);
+            Console.ReadLine();
+        }
+
+        static void Resize<T>(ref T[] arr1, int length)
+        {
+            T[] arr2 = new T[length];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr2[i] = arr1[i];
+            }
+            arr1 = arr2;
+        }
+    }
+}
+
+
+
